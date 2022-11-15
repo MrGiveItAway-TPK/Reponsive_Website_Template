1d9c7208020df49717bf11f7e090687193cea8a1
Munes Bani Fawaz <100308806+MrGiveItAway-TPK@users.noreply.github.com>
2022-11-15 18:57:49 +0300

    initial-commit

---
+++ js/pages_loaders/index.js
@@ -0,0 +1,8 @@
+$("#header_section").load("html_includes/header.html");
+$("#content_section").load("html_includes/content.html"); 
+$("#footer_section").load("html_includes/footer.html"); 
+$( document ).ready(function() {
+  $.getScript('js/hamburger_menu.js');
+});
+
+ 
